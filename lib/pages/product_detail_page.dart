4df390da710877233ac0b0e8dc4bdd4cfc9eade5
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-02-25 00:20:28 -0300

    Add Product Detail

---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:e_shop/models/products.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  const ProductDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Product product =
+        ModalRoute.of(context)?.settings.arguments as Product;
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(product.title),
+        ),
+      ),
+    );
+  }
+}
