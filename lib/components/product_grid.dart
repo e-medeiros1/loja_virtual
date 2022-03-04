359db27b08a06bddd4f23806c984b757086ea803
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-04 00:34:44 -0300

    Adds changeNotifierProvider

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -7,13 +7,13 @@ import 'package:provider/provider.dart';
 class ProductGrid extends StatelessWidget {
   const ProductGrid({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-        //Acessando lista de produtos
+    //Acessando lista de produtos
     final provider = Provider.of<ProductList>(context);
     final List<Product> loadedProducts = provider.items;
     return GridView.builder(
+      
       padding: const EdgeInsets.all(10),
       itemCount: loadedProducts.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -23,7 +23,11 @@ class ProductGrid extends StatelessWidget {
         mainAxisSpacing: 10,
       ),
       itemBuilder: (ctx, i) {
-        return ProductItem(products: loadedProducts[i]);
+        return ChangeNotifierProvider(
+          child: ProductItem(            
+          ),
+          create: (_) => loadedProducts[i],
+        );
       },
     );
   }
