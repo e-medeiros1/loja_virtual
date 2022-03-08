0a2969d5237ec65bbf4c76cd7a4b647b6de3234a
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-07 22:55:18 -0300

    Adds ternary operator to cart icon

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,26 +1,65 @@
 import 'package:e_shop/components/product_grid.dart';
+import 'package:e_shop/models/product_list.dart';
+import 'package:e_shop/models/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ProductsOverviewPage extends StatelessWidget {
+enum FilterOptions { Favorite, All }
+
+class ProductsOverviewPage extends StatefulWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsOverviewPage> createState() => _ProductsOverviewPageState();
+}
+
+class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
+  bool _showFavoriteOnly = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
+        centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.more_vert,
+              color: Colors.black,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {});
+              if (selectedValue == FilterOptions.Favorite) {
+                _showFavoriteOnly = true;
+              } else {
+                _showFavoriteOnly = false;
+              }
+            },
+          ),
+        ],
         elevation: 0,
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Center(
             child: Text(
-          'My e-Store',
+          'Minha loja virtual',
           style: TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
         )),
       ),
-      body: const ProductGrid(),
+      body:  ProductGrid(showFavoriteOnly: _showFavoriteOnly),
     );
   }
 }
