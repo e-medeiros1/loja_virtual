4091fd111055f9f5182a38197a3bfcc362dba345
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-08 22:26:01 -0300

    Adds new instance of provider to cart

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,3 +1,4 @@
+import 'package:e_shop/models/cart.dart';
 import 'package:e_shop/models/products.dart';
 import 'package:e_shop/others/app_routes.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,22 +28,22 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black54,
+          //Cart icon
           leading: Consumer<Product>(
             builder: (ctx, product, _) => IconButton(
                 color: Theme.of(context).colorScheme.secondary,
                 onPressed: () {
-                  product.toggleCart();
+                  cart.addItem(product);
                 },
-                icon: Icon(
-                  product.isFull
-                      ? Icons.local_grocery_store
-                      : Icons.local_grocery_store_outlined,
+                icon: const Icon(
+                  Icons.add_shopping_cart,
                 )),
           ),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
           ),
+          //Favorite Icon
           trailing: Consumer<Product>(
             builder: (ctx, product, _) => IconButton(
               color: Theme.of(context).colorScheme.secondary,
