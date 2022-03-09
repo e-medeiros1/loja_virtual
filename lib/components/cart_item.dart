a2dafdddf0992fc855f1f337c72c1bd4875535c7
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-09 11:18:03 -0300

    Create CartItem Widget

---
+++ lib/components/cart_item.dart
@@ -0,0 +1,12 @@
+import 'package:e_shop/models/cart_item.dart';
+import 'package:flutter/material.dart';
+
+class CartItemWidget extends StatelessWidget {
+  final CartItem cartItem;
+  const CartItemWidget({Key? key, required this.cartItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(cartItem.name);
+  }
+}
