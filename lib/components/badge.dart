2060f9a5605d5784111a907aa4262c84a9e60f19
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-08 22:25:24 -0300

    Widget to show the quantity of items in the cart

---
+++ lib/components/badge.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Badge extends StatelessWidget {
+  final Widget child;
+  final String value;
+  final Color? color;
+
+  const Badge({
+    Key? key,
+    required this.child,
+    required this.value,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 8,
+          right: 0,
+          child: Container(
+            padding: const EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: color ?? Colors.black,
+            ),
+            constraints: const BoxConstraints(
+              minHeight: 15,
+              minWidth: 15,
+            ),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 11,
+                  fontWeight: FontWeight.w800),
+            ),
+          ),
+        ),
+        child,
+      ],
+    );
+  }
+}
