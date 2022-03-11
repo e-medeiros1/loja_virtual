5957d6797ceb045d2ed95168d0d725e6a3f8e8fb
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-11 12:14:42 -0300

    Order custom widget construction

---
+++ lib/components/order.dart
@@ -0,0 +1,71 @@
+import 'package:e_shop/models/order.dart';
+import 'package:e_shop/models/products.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class OrderWidget extends StatefulWidget {
+  final Order order;
+  const OrderWidget({Key? key, required this.order}) : super(key: key);
+
+  @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle:
+                Text(DateFormat('M/dd/yyyy h:mm').format(widget.order.date)),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: const Icon(
+                Icons.expand_more,
+              ),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25) + 10,
+              child: ListView(
+                children: widget.order.products.map((products) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        products.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${products.quantity}x R\$ ${products.price}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
