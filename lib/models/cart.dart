80cdf123ec2c49251a5c08d82c742538379885b5
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-11 23:23:53 -0300

    Adds removeSingleItem method

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -58,6 +58,28 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (items[productId]?.quantity == 1) {
+      _items.remove(productId);
+    } else{
+            _items.update(
+        productId,
+        (existingItem) => CartItem(
+          id: existingItem.id,
+          productId: existingItem.productId,
+          name: existingItem.name,
+          quantity: existingItem.quantity - 1,
+          price: existingItem.price,
+        ),
+      );
+    }
+
+    notifyListeners();
+  }
+
 //Clear items
   void clear() {
     _items = {};
