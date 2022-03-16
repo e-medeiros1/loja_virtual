b39ee8f7df75e5ed01bab5fa156c0d8659ac8418
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-16 11:53:36 -0300

    Adds all form validations

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:e_shop/models/product_list.dart';
 import 'package:e_shop/models/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({Key? key}) : super(key: key);
@@ -40,6 +42,15 @@ class _ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
+  bool isValidImageUrl(String url) {
+    bool isValidUrl = Uri.tryParse(url)?.hasAbsolutePath ?? false;
+    bool endsWithFile = url.toLowerCase().endsWith('.png') ||
+        url.toLowerCase().endsWith('.jpg') ||
+        url.toLowerCase().endsWith('.jpeg');
+
+    return isValidUrl && endsWithFile;
+  }
+
   void _submitForm() {
     //Método de validação
     final isValid = _formKey.currentState?.validate() ?? false;
@@ -57,17 +68,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
       price: _formData['price'] as double,
       imageUrl: _formData['imageUrl'] as String,
     );
-
-    print(newProduct.id);
-    print(newProduct.title);
-    print(newProduct.price);
+    //Temos acesso ao contexto no statefull a partir do momento em que estamos
+    //em uma clase State
+    Provider.of<ProductList>(context, listen: false).addProduct(newProduct);
+    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Formulário de Produto'),
+        centerTitle: true,
+        title: const Text(
+          'Formulário de Produto',
+          textAlign: TextAlign.center,
+        ),
         actions: [
           IconButton(
             onPressed: _submitForm,
@@ -81,6 +96,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
           key: _formKey,
           child: ListView(
             children: [
+              //FormField NOME
               TextFormField(
                 decoration: const InputDecoration(
                     labelText: 'Nome', hintText: 'Ex. Camisa'),
@@ -89,13 +105,17 @@ class _ProductFormPageState extends State<ProductFormPage> {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
                 onSaved: (name) => _formData['name'] = name ?? '',
+
+                //Dica para as validações: Tentar sempre transformar em um método
+                //para não ter que ficar digitando a mesma coisa em todo TextField
+                //A ideia é sempre promover o reuso
                 validator: (_name) {
                   final name = _name ?? '';
-//Verifica se o nome está vazio ou somente com espaços
+                  //Verifica se o nome está vazio ou somente com espaços
                   if (name.trim().isEmpty) {
                     return 'Nome obrigatório';
                   }
-//Verifica o tamanho do nome
+                  //Verifica o tamanho do nome
                   if (name.trim().length < 3) {
                     return 'Nome deve conter no mínimo 3 caracteres';
                   }
@@ -105,6 +125,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
                   return null;
                 },
               ),
+              //FormField PREÇO
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Preço'),
                 textInputAction: TextInputAction.next,
@@ -117,7 +138,17 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 },
                 onSaved: (price) =>
                     _formData['price'] = double.parse(price ?? '0'),
+                validator: (_price) {
+                  final priceString = _price ?? '';
+                  final price = double.tryParse(priceString) ?? -1;
+
+                  if (price <= 0) {
+                    return 'Informe um preço válido';
+                  }
+                  return null;
+                },
               ),
+              //FormField DESCRIÇÃO
               TextFormField(
                 decoration: const InputDecoration(
                     labelText: 'Descrição', hintText: 'Ex. Camisa listrada'),
@@ -127,11 +158,27 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 maxLines: 1,
                 onSaved: (description) =>
                     _formData['description'] = description ?? '',
+                validator: (_description) {
+                  final description = _description ?? '';
+                  //Verifica se o nome está vazio ou somente com espaços
+                  if (description.trim().isEmpty) {
+                    return 'Descrição obrigatória';
+                  }
+                  //Verifica o tamanho do nome
+                  if (description.trim().length < 10) {
+                    return 'Descrição deve conter no mínimo 10 caracteres';
+                  }
+
+                  //Null significa que deu tudo certo
+                  //Se retornar string, essa será a mensagem de erro
+                  return null;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
+                    //FormField URL
                     child: TextFormField(
                       decoration: const InputDecoration(
                           labelText: 'Url da Imagem', hintText: 'Ex. http://'),
@@ -142,6 +189,13 @@ class _ProductFormPageState extends State<ProductFormPage> {
                       onFieldSubmitted: (_) => _submitForm(),
                       onSaved: (imageUrl) =>
                           _formData['imageUrl'] = imageUrl ?? '',
+                      validator: (_imageUrl) {
+                        final imageUrl = _imageUrl ?? '';
+                        if (!isValidImageUrl(imageUrl)) {
+                          return 'Informe uma url válida';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   Container(
