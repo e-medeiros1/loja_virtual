264eadfb074cf487f3f739b588850641b171c81f
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-25 21:53:23 -0300

    Adds and implement Firebase to Form

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:e_shop/data/dummy_data.dart';
 import 'package:e_shop/models/products.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
 //Retornando a referência da lista de produtos, onde quem tiver acesso ao get pode ter acesso a
@@ -10,6 +12,7 @@ class ProductList with ChangeNotifier {
 //List<Product> get items => _items;
 
 //Retornando um clone do items(Forma correta)
+  final _baseUrl = 'https://loja-virtual-343de-default-rtdb.firebaseio.com';
   List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -20,7 +23,7 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
-  void saveProduct(Map<String, Object> data) {
+ Future<void>  saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final newProduct = Product(
@@ -31,20 +34,42 @@ class ProductList with ChangeNotifier {
       imageUrl: data['imageUrl'] as String,
     );
     if (hasId) {
-      updateProduct(newProduct);
+      return updateProduct(newProduct);
     } else {
-      addProduct(newProduct);
+      return addProduct(newProduct);
     }
   }
 
 //Adiciona produto
-  void addProduct(Product product) {
-    _items.add(product);
-    //Parte essencial do fluxo de programação reativa
-    notifyListeners();
+  Future<void> addProduct(Product product) {
+    //Encode do Json
+    final future = http.post(Uri.parse('$_baseUrl/products.json'),
+        body: jsonEncode(
+          {
+            "name": product.title,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+            "isFavorite": product.isFavorite,
+          },
+        ));
+    //manda o produto novo pro servidor e depois aparece na lista de produtos
+    return future.then<void>((response) {
+      //Decode do Json
+      final id = jsonDecode(response.body)['name'];
+      _items.add(Product(
+          id: id,
+          description: product.description,
+          imageUrl: product.imageUrl,
+          title: product.title,
+          price: product.price,
+          isFavorite: product.isFavorite));
+      //Parte essencial do fluxo de programação reativa
+      notifyListeners();
+    });
   }
 
-  void updateProduct(Product product) {
+  Future<void> updateProduct(Product product) {
     //Verifica se o index é válido, caso não encontre, retorna -1, que não é um valor int aceitável
     int index = _items.indexWhere((p) => p.id == product.id);
 
@@ -52,7 +77,10 @@ class ProductList with ChangeNotifier {
       _items[index] = product;
       notifyListeners();
     }
+
+    return Future.value();
   }
+
   //Método de remoção de produto
   void removeProduct(Product product) {
     int index = _items.indexWhere((p) => p.id == product.id);
