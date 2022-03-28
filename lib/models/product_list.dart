c66348a832e592970ec80de4123f67841f26119f
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-28 17:06:23 -0300

    Adds loadProduct to receive info from database

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,19 +1,19 @@
 import 'dart:convert';
 import 'dart:math';
 
-import 'package:e_shop/data/dummy_data.dart';
 import 'package:e_shop/models/products.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-class ProductList with ChangeNotifier {
 //Retornando a referência da lista de produtos, onde quem tiver acesso ao get pode ter acesso a
 //outras propriedades do método
-//List<Product> get items => _items;
+// List<Product> get items => _items;
 
 //Retornando um clone do items(Forma correta)
-  final _baseUrl = 'https://loja-virtual-343de-default-rtdb.firebaseio.com';
-  List<Product> _items = dummyProducts;
+class ProductList with ChangeNotifier {
+  final _url =
+      'https://loja-virtual-343de-default-rtdb.firebaseio.com/products.json';
+  List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -23,7 +23,28 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
- Future<void>  saveProduct(Map<String, Object> data) {
+  Future<void> loadProducts() async {
+    _items.clear();
+
+    final response = await http.get(Uri.parse(_url));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          title: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
+  }
+
+  Future<void> saveProduct(Map<String, Object> data) {
     bool hasId = data['id'] != null;
 
     final newProduct = Product(
@@ -41,9 +62,9 @@ class ProductList with ChangeNotifier {
   }
 
 //Adiciona produto
-  Future<void> addProduct(Product product) {
+  Future<void> addProduct(Product product) async {
     //Encode do Json
-    final future = http.post(Uri.parse('$_baseUrl/products.json'),
+    final response = await http.post(Uri.parse(_url),
         body: jsonEncode(
           {
             "name": product.title,
@@ -53,20 +74,19 @@ class ProductList with ChangeNotifier {
             "isFavorite": product.isFavorite,
           },
         ));
+
+    //Decode do Json
+    final id = jsonDecode(response.body)['name'];
+    _items.add(Product(
+        id: id,
+        description: product.description,
+        imageUrl: product.imageUrl,
+        title: product.title,
+        price: product.price,
+        isFavorite: product.isFavorite));
+    //Parte essencial do fluxo de programação reativa
+    notifyListeners();
     //manda o produto novo pro servidor e depois aparece na lista de produtos
-    return future.then<void>((response) {
-      //Decode do Json
-      final id = jsonDecode(response.body)['name'];
-      _items.add(Product(
-          id: id,
-          description: product.description,
-          imageUrl: product.imageUrl,
-          title: product.title,
-          price: product.price,
-          isFavorite: product.isFavorite));
-      //Parte essencial do fluxo de programação reativa
-      notifyListeners();
-    });
   }
 
   Future<void> updateProduct(Product product) {
