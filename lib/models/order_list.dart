1ffd46ef0e52225f5fb057d75748352fae812ee8
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-31 17:52:23 -0300

    Integrate order list with database

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:math';
-
 
 import 'package:e_shop/models/cart.dart';
 import 'package:e_shop/models/order.dart';
+import 'package:e_shop/others/constants.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class OrderList with ChangeNotifier {
   List<Order> _items = [];
@@ -16,13 +18,32 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  void addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final response =
+        await http.post(Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+            body: jsonEncode(
+              {
+                'total': cart.totalAmount,
+                'date': date.toIso8601String(),
+                'products': cart.items.values
+                    .map((cartItem) => {
+                          'id': cartItem.id,
+                          'productId': cartItem.productId,
+                          'name': cartItem.name,
+                          'quantity': cartItem.quantity,
+                          'price': cartItem.price,
+                        })
+                    .toList(),
+              },
+            ));
+    final id = jsonDecode(response.body)['name'];
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount!,
-        date: DateTime.now(),
+        date: date,
         products: cart.items.values.toList(),
       ),
     );
