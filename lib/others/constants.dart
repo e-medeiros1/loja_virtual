a40c12b5870f90be8324daa921edec10e1aef8ba
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-31 17:52:47 -0300

    Set new contanst value to order base url

--- lib/others/constants.dart
+++ lib/others/constants.dart
@@ -1,3 +1,4 @@
 class Constants {
   static const PRODUCT_BASE_URL = 'https://loja-virtual-343de-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL = 'https://loja-virtual-343de-default-rtdb.firebaseio.com/orders';
 }
