dc92e3382fce371b3caf868fb856452c41570e5c
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-03-30 23:52:56 -0300

    Connect favorite method with firebase

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:e_shop/others/constants.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -7,10 +11,8 @@ class Product with ChangeNotifier {
   final String imageUrl;
   final double price;
   bool isFavorite;
- 
 
   Product({
-
     required this.id,
     required this.description,
     required this.imageUrl,
@@ -19,10 +21,23 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     notifyListeners();
   }
 
-
+  Future<void> toggleFavorite() async {
+    _toggleFavorite();
+    try {
+      final response = await http.patch(
+          Uri.parse('${Constants.PRODUCT_BASE_URL}/$id.json'),
+          body: jsonEncode({"isFavorite": isFavorite}));
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+        notifyListeners();
+      }
+    } catch (_) {
+      _toggleFavorite();
+    }
+  }
 }
