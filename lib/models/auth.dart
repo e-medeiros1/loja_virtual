fd3178ad5648fac12b79147724b92649940af76f
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-04-07 10:30:19 -0300

    Adds Token management

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,8 +5,30 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBNvjPWzm0G1FydYhM5OS4chndxmjwP8ds';
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expireDate;
+//Recebendo token depois do submit
+
+  bool get isAuth {
+    //Verifica se a data é depois da atual
+    final isValid = _expireDate?.isAfter(DateTime.now()) ?? false;
+    //Infos necessárias para saber se o user está autenticado
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
 
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
@@ -27,9 +49,16 @@ class Auth with ChangeNotifier {
 
     if (body['error'] != null) {
       throw AuthException(key: body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
+      //Salva a data atual + uma duração de 3600 segundos
+      _expireDate = DateTime.now().add(Duration(
+        seconds: int.parse(body['expiresIn']),
+      ));
+      notifyListeners();
     }
-
-    print(body);
   }
 
   Future<void> signup(String email, String password) async {
