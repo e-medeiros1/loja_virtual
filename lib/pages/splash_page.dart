028aaa37cb7cc3581603fab1e6b6bb982cd76282
e-medeiros1 <73318684+e-medeiros1@users.noreply.github.com>
2022-04-13 00:08:24 -0300

    Adds animation on splash screen icon with a lib

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,7 @@
 import 'package:e_shop/others/app_routes.dart';
+import 'package:e_shop/pages/auth_or_home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,24 +11,30 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2)).then(
-      (_) => Navigator.of(context).pushReplacementNamed(AppRoutes.AUTH_OR_HOME),
-    );
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(const Duration(seconds: 2)).then(
+  //     (_) => Navigator.of(context).pushReplacementNamed(AppRoutes.AUTH_OR_HOME),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-          child: Image.asset(
-        'assets/images/logo.png',
-        fit: BoxFit.cover,
-        scale: 2,
-      )),
+    return AnimatedSplashScreen(
+      splashIconSize: 300,
+      nextScreen: const AuthOrHomePage(),
+      splashTransition: SplashTransition.scaleTransition,
+      duration: 200,
+      splash: Container(
+        color: Colors.white,
+        child: Center(
+            child: Image.asset(
+          'assets/images/logo.png',
+          fit: BoxFit.cover,
+          scale: 2,
+        )),
+      ),
     );
   }
 }
